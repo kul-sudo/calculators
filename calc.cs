ba4fd4770a5f2f432e39b22956facd11cfafbe90
Mai <interacsion@protonmail.com>
2026-03-14 16:42:39 +0100

    c#

--- calc.cs
+++ calc.cs
@@ -1,48 +1,41 @@
 #!/usr/bin/env dotnet
 
-public class Calc {
-	public static void Main(String[] args) {
-		while (true) {
-			var line = Console.ReadLine();
-			if(!string.IsNullOrEmpty(line))
-				ProcessLine(line);
-			else 
-				Console.WriteLine("Too Few Arguments");
+void ProcessLine(string stdLine) {
+	var stack = new Stack<int>();
+
+	foreach (var e in stdLine.Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)) {
+		if (int.TryParse(e, out int parseResult)) {
+			stack.Push(parseResult);
+			continue;
 		}
-	}
 
-	static void ProcessLine(string stdLine) {
-		var stack = new Stack<int>();
+		if (stack.Count < 2) {
+			Console.WriteLine("Too Few Arguments");
+			return;
+		}
 
-		foreach (var e in stdLine.Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)) {
-			if(int.TryParse(e, out int parseResult)){
-				stack.Push(parseResult);
-				continue;
-			} 
-			
-
-			if (stack.Count < 2) {
-				Console.WriteLine("Too Few Arguments");
+		var b = stack.Pop();
+		var a = stack.Pop();
+		
+		switch (e) {
+			case "+": stack.Push(a + b); break;
+			case "-": stack.Push(a - b); break;
+			case "*": stack.Push(a * b); break;
+			case "/": stack.Push(a / b); break;
+			default:
+				Console.WriteLine("Invalid Token");
 				return;
-			}
-
-			var b = stack.Pop();
-			var a = stack.Pop();
-			
-			switch (e) {
-				case "+": stack.Push(a + b); break;
-				case "-": stack.Push(a - b); break;
-				case "*": stack.Push(a * b); break;
-				case "/": stack.Push(a / b); break;
-				default:
-					Console.WriteLine("Invalid Token");
-					return;
-			}
 		}
-
-		if (stack.Count == 1)
-			Console.WriteLine(stack.Pop());
-		else
-			Console.WriteLine("Invalid Input");
 	}
+
+	if (stack.Count == 1)
+		Console.WriteLine(stack.Pop());
+	else
+		Console.WriteLine("Invalid Input");
+}
+
+while (true) {
+	var line = Console.ReadLine();
+	if (line is null) break;
+	ProcessLine(line);
 }
